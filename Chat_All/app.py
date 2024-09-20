4590c8b9f18bb6f6f5dc3914e3ca3781c00f4951
VirtualMachine01 <tempvm12@gmail.com>
2024-09-20 15:06:23 +0530

    chatbot with streamlit

---
+++ Chat_All/app.py
@@ -0,0 +1,83 @@
+import streamlit as st
+from llm_chains import load_normal_chain
+from utils import save_chat_history_json, load_chat_history_json,get_timestamp
+from langchain_community.chat_message_histories import StreamlitChatMessageHistory
+import yaml
+import os
+with open("config.yaml", "r") as f:
+    config = yaml.safe_load(f)
+
+def load_chain(chat_history):
+    return load_normal_chain(chat_history)
+
+def clear_input_field():
+    st.session_state.user_question = st.session_state.user_input
+    st.session_state.user_input = ""
+
+def set_send_input():
+    st.session_state.send_input = True
+    clear_input_field()
+
+def track_index():
+    st.session_state.session_index_tracker = st.session_state.session_key
+
+def save_chat_history():
+    if st.session_state.history != []:
+        if st.session_state.session_key == "new_session":
+            st.session_state.new_session_key = get_timestamp() + ".json"
+            save_chat_history_json(st.session_state.history, config["chat_history_path"] + st.session_state.new_session_key)
+        else:
+            save_chat_history_json(st.session_state.history, config["chat_history_path"] + st.session_state.session_key)
+
+def main():
+    st.title("Chitra Vani")
+    # st.write(css, unsafe_allow_html=True)
+    chat_container = st.container()
+    st.sidebar.title("Chat Sessions")
+    chat_sessions = ["new_session"] + os.listdir(config["chat_history_path"])
+
+    if "send_input" not in st.session_state:
+        st.session_state.session_key = "new_session"
+        st.session_state.send_input = False
+        st.session_state.user_question = ""
+        st.session_state.new_session_key = None
+        st.session_state.session_index_tracker = "new_session"
+    if st.session_state.session_key == "new_session" and st.session_state.new_session_key != None:
+        st.session_state.session_index_tracker = st.session_state.new_session_key
+        st.session_state.new_session_key = None
+
+    index = chat_sessions.index(st.session_state.session_index_tracker)
+    st.sidebar.selectbox("Select a Session", chat_sessions, key="session_key", index=index, on_change=track_index)
+
+    if st.session_state.session_key != "new_session":
+        st.session_state.history = load_chat_history_json(config["chat_history_path"] + st.session_state.session_key)
+    else:
+        st.session_state.history = []
+
+    chat_history = StreamlitChatMessageHistory(key="history")
+    llm_chain = load_chain(chat_history)
+
+
+    user_input = st.text_input("Type your question here", key="user_input", on_change=set_send_input)
+
+    send_button = st.button("Ask", key="send_button")
+
+    if send_button or st.session_state.send_input:
+        if st.session_state.user_question != "":
+            with chat_container:
+                # st.chat_message("user").write(st.session_state.user_question)
+                llm_response = llm_chain.run(st.session_state.user_question)
+                # st.chat_message("ai").write(llm_response['text'])
+                st.session_state.user_question = ""
+
+    
+    if chat_history.messages != []:
+        with chat_container:
+            st.write("Chat History : ")
+            for msg in chat_history.messages:
+                st.chat_message(msg.type).write(msg.content)
+    save_chat_history()
+    
+
+if __name__ == "__main__":
+    main()
